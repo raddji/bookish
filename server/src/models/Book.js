bbe822f7bd9aa995e8a9a3df8d9d6240d3cefeca
Raddi Hansen <raddji@gmail.com>
2022-09-28 20:01:03 -0400

    add uniqueness to model

--- server/src/models/Book.js
+++ server/src/models/Book.js
@@ -1,9 +1,25 @@
-const Model = require("./Model");
+const Model = require('objection').Model;
 
-class Book extends Model {
+const unique = require('objection-unique')({
+  fields: ['title', 'author'],
+  identifiers: ['id']
+})
+
+class Book extends unique(Model) {
   static get tableName() {
     return "books";
   };
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["title", "author"],
+      properties: {
+        title: { type: "string", minLength: 1, maxLength: 50 },
+        author: { type: "string", minLength: 1, maxLength: 30 }
+      }
+    }
+  }
 }
 
 module.exports = Book;
